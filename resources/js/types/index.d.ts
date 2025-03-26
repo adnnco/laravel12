4e3d0daab3c243a4eada7d020d9520dacc500dc1
Adnan Günay <adnnco@gmail.com>
2025-03-26 15:15:38 +0100

    team table is creating

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -40,3 +40,12 @@ export interface User {
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
 }
+
+export interface Team {
+    id: number;
+    name: string;
+    slug: string;
+    created_at: string;
+    updated_at: string;
+    [key: string]: unknown; // This allows for additional properties...
+}
